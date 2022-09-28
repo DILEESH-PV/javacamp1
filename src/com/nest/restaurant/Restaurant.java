a973b6ae0171d0d04495d861b1f336f6bc7cf611
DILEESH-PV <dileeshpv1998@gmail.com>
2022-09-28 10:56:05 +0530

    Billing Completed

--- src/com/nest/restaurant/Restaurant.java
+++ src/com/nest/restaurant/Restaurant.java
@@ -19,79 +19,99 @@ public class Restaurant {
         ArrayList<String> transactionlist=new ArrayList<>();
         Random ra=new Random();
         while (true) {
-            System.out.println("Select an option");
-            System.out.println("1. Dine Inn");
-            System.out.println("2. Take Away");
-            System.out.println("3. View all transaction");
-            System.out.println("4. EXIT");
+            System.out.println("*************************************");
+            System.out.println("*************************************");
+            System.out.println("   Select an option");
+            System.out.println("   1. Dine Inn");
+            System.out.println("   2. Take Away");
+            System.out.println("   3. View all transaction");
+            System.out.println("   4. EXIT");
             choice1 = scanner.nextInt();
             switch (choice1) {
                 case 1:
-                    System.out.println("Select an option in dine inn");
-                    System.out.println("1.Tea                   10Rs");
-                    System.out.println("2.Coffee                15Rs");
-                    System.out.println("3.Juice                 20Rs");
-                    System.out.println("4.Sandwich              50Rs");
-                    System.out.println("5.Burger                60Rs");
-                    System.out.println("6.****** GENERATE BILL *****");
-                    System.out.println("7.EXIT                      ");
+                    System.out.println("*************************************");
+                    System.out.println("*************************************");
+                    System.out.println("   Select an option in dine inn");
+                    System.out.println("   1.Tea ------------------10Rs");
+                    System.out.println("   2.Coffee ---------------15Rs");
+                    System.out.println("   3.Juice ----------------20Rs");
+                    System.out.println("   4.Sandwich -------------50Rs");
+                    System.out.println("   5.Burger ---------------60Rs");
+                    System.out.println("   6.****** GENERATE BILL *****");
+                    System.out.println("   7.EXIT                      ");
                     choice2=scanner.nextInt();
                     switch (choice2) {
                         case 1:
-                            System.out.println("Ënter the quantity");
+                            System.out.println("*************************************");
+                            System.out.println("   Enter The Quantity");
                             quantity = scanner.nextInt();
                             tquantity.add(quantity);
                             price.add(10);
-                            foodItem.add("Tea");
+                            foodItem.add("   Tea  -------");
                             total += quantity * foodItems.getTea();
                             break;
                         case 2:
-                            System.out.println("Ënter the quantity");
+                            System.out.println("*************************************");
+                            System.out.println("   Enter the quantity");
                             quantity = scanner.nextInt();
                             tquantity.add(quantity);
                             price.add(15);
-                            foodItem.add("Coffee");
+                            foodItem.add("   Coffee -----");
                             total += quantity * foodItems.getCoffee();
                             break;
                         case 3:
-                            System.out.println("Ënter the quantity");
+                            System.out.println("*************************************");
+                            System.out.println("   Enter the quantity");
                             quantity = scanner.nextInt();
                             tquantity.add(quantity);
                             price.add(20);
-                            foodItem.add("Juice");
+                            foodItem.add("   Juice ------");
                             total += quantity * foodItems.getJuice();
                             break;
                         case 4:
-                            System.out.println("Ënter the quantity");
+                            System.out.println("*************************************");
+                            System.out.println("   Enter the quantity");
                             quantity = scanner.nextInt();
                             tquantity.add(quantity);
                             price.add(50);
-                            foodItem.add("Sandwich");
+                            foodItem.add("   Sandwich ---");
                             total += quantity * foodItems.getSandwich();
                             break;
                         case 5:
-                            System.out.println("Ënter the quantity");
+                            System.out.println("*************************************");
+                            System.out.println(" Enter the quantity");
                             quantity = scanner.nextInt();
                             tquantity.add(quantity);
                             price.add(60);
-                            foodItem.add("Burger");
+                            foodItem.add("   Burger -----");
                             total += quantity * foodItems.getBurger();
                             break;
                         case 6:
-                            System.out.println("enter name");
+                            System.out.println("*************************************");
+                            System.out.println("  Please Enter Your Name");
                             name= scanner.next();
-                            for(int i=0;i<foodItem.size();i++) {
-                                System.out.println(foodItem.get(i) + "   :   " + price.get(i) + "Rs  *" + tquantity.get(i));
-                            }
-                            System.out.println("Total Bill= "+total);
+                            System.out.println("\n");
                             invoice= ra.nextInt(100);
-                            System.out.println("Invoice Number= "+invoice);
+                            System.out.println("                HOTEL HORIZON INTERNATIONAL");
+                            System.out.println("   Invoice Number ---- "+invoice);
+                            //System.out.println("\n");
+                            //System.out.println("*************************************");
+                            System.out.println("   Items              Price       Quantity\n\n");
+                            for(int i=0;i<foodItem.size();i++) {
+                                System.out.println(foodItem.get(i) + "------- " + price.get(i) + " Rs    *     " + tquantity.get(i));
+                            }
+                            System.out.println("\n");
+                            System.out.println("   Total Bill -------- "+total+" Rs");
+                            System.out.println("******* THANK YOU FOR VISITING *******");
+                            System.out.println("\n\n");
+
+
                             foodItem.clear();
                             price.clear();
                             tquantity.clear();
                             map.put("Name",name);
                             map.put("Total",String.valueOf(total));
-                            map.put("mode","Dine Inn");
+                            map.put("Mode","Dine Inn");
                             map.put("Invoice",String.valueOf(invoice));
                             transactionlist.add(String.valueOf(map));
                             total=0;
@@ -100,70 +120,79 @@ public class Restaurant {
                             System.exit(0);
                             break;
                         default:
-                            System.out.println("Please select a valid item");
+                            System.out.println("Please Select A Valid Option");
                             break;
                     }
                     break;
                 case 2:
-                    System.out.println("Select an option for Take away");
-                    System.out.println("1.Tea                     12Rs");
-                    System.out.println("2.Coffee                  20Rs");
-                    System.out.println("3.Juice                   25Rs");
-                    System.out.println("4.Sandwich                55Rs");
-                    System.out.println("5.Burger                  65Rs");
-                    System.out.println("6.******* GENERATE BILL ******");
-                    System.out.println("7.EXIT                        ");
+                    System.out.println("*************************************");
+                    System.out.println("*************************************");
+                    System.out.println("   Select An Option For Take away");
+                    System.out.println("   1.Tea --------------------12Rs");
+                    System.out.println("   2.Coffee -----------------20Rs");
+                    System.out.println("   3.Juice ------------------25Rs");
+                    System.out.println("   4.Sandwich ---------------55Rs");
+                    System.out.println("   5.Burger -----------------65Rs");
+                    System.out.println("   6.******* GENERATE BILL ******");
+                    System.out.println("   7.EXIT                        ");
                     choice2=scanner.nextInt();
                     switch (choice2) {
                         case 1:
-                            System.out.println("Ënter the quantity");
+                            System.out.println("   Enter The Quantity");
                             quantity = scanner.nextInt();
                             tquantity.add(quantity);
-                            foodItem.add("Tea");
+                            foodItem.add("   Tea  -------");
                             price.add(takeAwayFood.getTea());
                             total += quantity * takeAwayFood.getTea();
                             break;
                         case 2:
-                            System.out.println("Ënter the quantity");
+                            System.out.println("   Enter The Quantity");
                             quantity = scanner.nextInt();
                             tquantity.add(quantity);
-                            foodItem.add("Coffee");
+                            foodItem.add("   Coffee -----");
                             price.add(takeAwayFood.getCoffee());
                             total += quantity * takeAwayFood.getCoffee();
                             break;
                         case 3:
-                            System.out.println("Ënter the quantity");
+                            System.out.println("   Enter The Quantity");
                             quantity = scanner.nextInt();
                             tquantity.add(quantity);
-                            foodItem.add("Juice");
+                            foodItem.add("   Juice ------");
                             price.add(takeAwayFood.getJuice());
                             total += quantity * takeAwayFood.getJuice();
                             break;
                         case 4:
-                            System.out.println("Ënter the quantity");
+                            System.out.println("   Enter The Quantity");
                             quantity = scanner.nextInt();
                             tquantity.add(quantity);
-                            foodItem.add("Sandwich");
+                            foodItem.add("   Sandwich ---");
                             price.add(takeAwayFood.getSandwich());
                             total += quantity * takeAwayFood.getSandwich();
                             break;
                         case 5:
-                            System.out.println("Ënter the quantity");
+                            System.out.println("   Enter The Quantity");
                             quantity = scanner.nextInt();
                             tquantity.add(quantity);
-                            foodItem.add("Burger");
+                            foodItem.add("   Burger -----");
                             price.add(takeAwayFood.getBurger());
                             total += quantity * takeAwayFood.getBurger();
                             break;
                         case 6:
-                            System.out.println("Enter your name");
+                            System.out.println("   Please Enter Your Name");
                             name= scanner.next();
-                            for(int i=0;i<foodItem.size();i++) {
-                                System.out.println(foodItem.get(i) + "   :   " + price.get(i) + "Rs  *" + tquantity.get(i));
-                            }
-                            System.out.println("Total Bill= "+total);
+                            System.out.println("\n");
                             invoice= ra.nextInt(100);
-                            System.out.println("Invoice Number= "+invoice);
+                            System.out.println("                HOTEL HORIZON INTERNATIONAL");
+                            System.out.println("   Invoice Number ---- "+invoice);
+                            System.out.println("   Items              Price       Quantity\n\n");
+                            for(int i=0;i<foodItem.size();i++) {
+                                System.out.println(foodItem.get(i) + "------- " + price.get(i) + " Rs    *     " + tquantity.get(i));
+                            }
+                            System.out.println("\n");
+                            System.out.println("   Total Bill -------- "+total+" Rs");
+                            //System.out.println("");
+                            System.out.println("******* THANK YOU FOR VISITING *******");
+                            System.out.println("\n\n");
                             foodItem.clear();
                             price.clear();
                             tquantity.clear();
@@ -182,7 +211,7 @@ public class Restaurant {
                             break;
                     }break;
                 case 3:
-                    System.out.println("****** The Total Transaction is ******");
+                    System.out.println("****** The Total Transaction is ******\n");
                     System.out.println(transactionlist);
                     break;
                     case 4:
